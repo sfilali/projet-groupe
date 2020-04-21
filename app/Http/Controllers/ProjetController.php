752e4db25f6df71402fa2a3a80e02a9fbf00cc22
Ayhan <caliskanayhan@hotmail.frgit config --global user.naimeayhangit config --global user.email caliskanayhan@hotmail.fr>
2020-04-21 15:51:23 +0200

    Function index

--- app/Http/Controllers/ProjetController.php
+++ app/Http/Controllers/ProjetController.php
@@ -10,7 +10,8 @@ class ProjetController extends Controller
 
     public function index()
     {
-        //
+        $projets = Projet::all();
+        return view("projets", compact('projets'));
     }
 
 
