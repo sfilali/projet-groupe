5fe941bb6365739935902ab4298757f65f5a7f72
sfilali <soumaya-filali@hotmail.be>
2020-04-21 15:35:32 +0200

    Route::resource

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::resource('/users', 'UserController');
+Route::resource('/projets', 'ProjetController');
+Route::resource('/taches', 'TacheController');
