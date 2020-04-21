b0018b67cce389ce352fba6d75260733e88808aa
Ayhan <caliskanayhan@hotmail.frgit config --global user.naimeayhangit config --global user.email caliskanayhan@hotmail.fr>
2020-04-21 12:32:15 +0200

    Creation Projet model -mcr

---
+++ app/Http/Controllers/ProjetController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Projet;
+use Illuminate\Http\Request;
+
+class ProjetController extends Controller
+{
+
+    public function index()
+    {
+        //
+    }
+
+
+    public function create()
+    {
+        //
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show(Projet $projet)
+    {
+        //
+    }
+
+ 
+    public function edit(Projet $projet)
+    {
+        //
+    }
+
+
+    public function update(Request $request, Projet $projet)
+    {
+        //
+    }
+
+ 
+    public function destroy(Projet $projet)
+    {
+        //
+    }
+}
