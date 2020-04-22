d9751c2f22e1ade50fd3fdc55927d55f4f90e196
sfilali <soumaya-filali@hotmail.be>
2020-04-22 11:43:10 +0200

    User & Tache:  Edit blade

--- resources/views/taches/edit.blade.php
+++ resources/views/taches/edit.blade.php
@@ -1,5 +1,40 @@
 @extends('layouts.index')
 
 @section('content')
-    <h1>Hheelo</h1>
+    
+<h2 class="text-center pt-5">Tache Edit</h2>
+<div  class="d-flex justify-content-center m-5">
+
+	<div  class="w-100 border bg-light p-5">
+        <form  action="/taches/{{$tache->id}}"  method="POST" >
+            @csrf
+            @method('put')
+
+                <div  class="form-group">
+                    <label  for="">Projet Name</label>
+                    <input  type="text" value={{$tache->name}} class="form-control"  name="tache_name"  id=""    placeholder="Name">
+                </div>
+            
+            
+                <div  class="form-group mb-4">
+                    <label  for="">Description</label>
+                    <input  type="text" class="form-control" value={{$tache->description}} name="description"  id=""  placeholder="Description">
+                </div>
+            
+                <div class="form-group">
+                    <label for="">Projet</label>
+                    <select name="projets_id" id="projets_id">
+                        @foreach ($projets as $projet)
+                            <option value="{{$projet->id}}">{{$projet->name}}</option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <div class="d-flex justify-content-center">
+                    <button  class="btn btn-success" type="submit">Upload</button>
+                </div>
+        </form>
+        
+	</div>
+</div>
 @endsection
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,5 +1,36 @@
 @extends('layouts.index')
 
 @section('content')
-    <h1>hello</h1>
+    
+<h2 class="text-center pt-5">User Edit</h2>
+<div  class="d-flex justify-content-center m-5">
+
+	<div  class="w-100 border bg-light p-5">
+        <form  action="/users/{{$user->id}}"  method="POST" >
+            @csrf
+            @method('put')
+
+                <div  class="form-group">
+                    <label  for="">Projet Name</label>
+                    <input  type="text" value={{$user->name}} class="form-control"  name="name"  id=""    placeholder="Name">
+                </div>
+            
+            
+                <div  class="form-group mb-4">
+                    <label  for="">email</label>
+                    <input  type="email" class="form-control" value={{$user->email}} name="email"  id=""  placeholder="Email">
+                </div>
+
+                <div  class="form-group mb-4">
+                    <label  for="">password</label>
+                    <input  type="text" class="form-control" value={{$user->password}} name="password"  id=""  placeholder="Password">
+                </div>
+
+                <div class="d-flex justify-content-center">
+                    <button  class="btn btn-success" type="submit">Upload</button>
+                </div>
+        </form>
+        
+	</div>
+</div>
 @endsection
